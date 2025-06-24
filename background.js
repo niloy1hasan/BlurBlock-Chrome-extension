80499d457ea883324e82fe160df68eeba1593982
niloy1hasan <notecse23@gmail.com>
2025-06-24 16:35:20 +0600

    hotkey added

--- background.js
+++ background.js
@@ -4,3 +4,16 @@ chrome.action.onClicked.addListener((tab) => {
     files: ["inject.js"]
   });
 });
+
+chrome.commands.onCommand.addListener((command) => {
+  if (command === "add-blur-mask") {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      if (tabs[0]?.id) {
+        chrome.scripting.executeScript({
+          target: { tabId: tabs[0].id },
+          files: ["inject.js"]
+        });
+      }
+    });
+  }
+});
